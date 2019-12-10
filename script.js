5ad807d9aaddf4a92e2c9e667ab8f39f6ccb710f
Nicolas Pierson <nicolas.pierson57@gmail.com>
2019-12-10 22:46:38 +0100

    commit avant test 2 tabs

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const aboutMeTab = document.querySelector("#aboutme");
 const projectsTab = document.querySelector("#myprojects");
-const contactTab = document.querySelector('#contact')
+const contactTab = document.querySelector("#contact")
 const aboutMeDiv = document.querySelector("#aboutme-div");
 const projectsDiv = document.querySelector("#projects-div");
 const contactDiv = document.querySelector("#contact-div");
-console.log(projectsDiv);
+console.log(contactTab);
 aboutMeTab.addEventListener('click', (event) => {
   aboutMeDiv.classList.remove('no-show');
   aboutMeDiv.classList.add('show');
   aboutMeTab.classList.add('bottom-border');
   projectsDiv.classList.remove('show');
   projectsTab.classList.remove('bottom-border');
+  contactTab.classList.remove('bottom-border');
   projectsDiv.classList.add('no-show');
+  contactDiv.classList.remove('show');
+  contactDiv.classList.add('no-show');
 })
 
 projectsTab.addEventListener('click', (event) => {
@@ -20,12 +23,20 @@ projectsTab.addEventListener('click', (event) => {
   projectsTab.classList.add('bottom-border');
   aboutMeDiv.classList.remove('show');
   aboutMeTab.classList.remove('bottom-border');
+  contactTab.classList.remove('bottom-border');
   aboutMeDiv.classList.add('no-show');
+  contactDiv.classList.remove('show');
+  contactDiv.classList.add('no-show');
 })
-
+console.log(contactTab);
 contactTab.addEventListener('click', (event) => {
-  aboutMeDiv.classList.remove('no-show');
-  aboutMeDiv.classList.add('show');
+  contactDiv.classList.remove('no-show');
+  contactDiv.classList.add('show');
+  aboutMeDiv.classList.add('no-show');
+  contactTab.classList.add('bottom-border');
+  aboutMeDiv.classList.remove('show');
+  aboutMeTab.classList.remove('bottom-border');
+  projectsTab.classList.remove('bottom-border');
   projectsDiv.classList.remove('show');
   projectsDiv.classList.add('no-show');
 })
