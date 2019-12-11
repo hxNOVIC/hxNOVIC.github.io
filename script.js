204a9402f8793ad8ffaabd343a129e5d26f50c9e
Nicolas Pierson <nicolas.pierson57@gmail.com>
2019-12-11 15:01:06 +0100

    mise en prod

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 const aboutMeTab = document.querySelector("#aboutme");
 const projectsTab = document.querySelector("#myprojects");
-const contactTab = document.querySelector("#contact")
 const aboutMeDiv = document.querySelector("#aboutme-div");
 const projectsDiv = document.querySelector("#projects-div");
-const contactDiv = document.querySelector("#contact-div");
-console.log(contactTab);
+
 aboutMeTab.addEventListener('click', (event) => {
   aboutMeDiv.classList.remove('no-show');
   aboutMeDiv.classList.add('show');
   aboutMeTab.classList.add('bottom-border');
   projectsDiv.classList.remove('show');
   projectsTab.classList.remove('bottom-border');
-  contactTab.classList.remove('bottom-border');
   projectsDiv.classList.add('no-show');
-  contactDiv.classList.remove('show');
-  contactDiv.classList.add('no-show');
-})
+});
 
 projectsTab.addEventListener('click', (event) => {
   projectsDiv.classList.remove('no-show');
@@ -23,20 +18,23 @@ projectsTab.addEventListener('click', (event) => {
   projectsTab.classList.add('bottom-border');
   aboutMeDiv.classList.remove('show');
   aboutMeTab.classList.remove('bottom-border');
-  contactTab.classList.remove('bottom-border');
   aboutMeDiv.classList.add('no-show');
-  contactDiv.classList.remove('show');
-  contactDiv.classList.add('no-show');
-})
-console.log(contactTab);
-contactTab.addEventListener('click', (event) => {
-  contactDiv.classList.remove('no-show');
-  contactDiv.classList.add('show');
-  aboutMeDiv.classList.add('no-show');
-  contactTab.classList.add('bottom-border');
-  aboutMeDiv.classList.remove('show');
-  aboutMeTab.classList.remove('bottom-border');
-  projectsTab.classList.remove('bottom-border');
-  projectsDiv.classList.remove('show');
-  projectsDiv.classList.add('no-show');
+});
+
+const at = document.querySelector('#at');
+at.addEventListener('mouseover', (event) => {
+  at.title = ("Click to copy - nicolas.pierson57@gmail.com");
+});
+
+const copyToClipboard = () => {
+  const el = document.createElement('textarea');
+  el.value = "nicolas.pierson57@gmail.com";
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+
+at.addEventListener('click', (event) => {
+  copyToClipboard();
 })
